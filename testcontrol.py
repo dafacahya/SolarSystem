f921fe9e2d5e70a62e789566ceb1ee1c2333bc96
root <root@DafaCahya>
2024-07-04 08:08:35 +0700

    change to wiringpi

--- testcontrol.py
+++ testcontrol.py
@@ -1,53 +1,89 @@
-import smbus
+import wiringpi
 import time
-from pyA20.gpio import gpio
-from pyA20.gpio import port
-
-# Inisialisasi SMBus untuk komunikasi I2C dengan MPU-6050
-bus = smbus.SMBus(1)  # Jika menggunakan Orange Pi, gunakan SMBus(0)
+from math import atan2, degrees, sin, cos
+import pandas as pd
+import numpy as np
 
 # Alamat I2C dari MPU-6050
 MPU6050_ADDR = 0x68
 
-# Register MPU-6050 untuk data akselerometer
+# Register MPU-6050 untuk data akselerometer dan gyroscope
 MPU6050_REG_ACCEL_XOUT_H = 0x3B
 MPU6050_REG_ACCEL_YOUT_H = 0x3D
 MPU6050_REG_ACCEL_ZOUT_H = 0x3F
 
 # Konfigurasi pin GPIO untuk relay
-RELAY_PIN_1 = port.PI0  # Ganti dengan pin GPIO yang sesuai
+RELAY_PIN_1 = 19  # Ganti dengan pin GPIO yang sesuai (misalnya BCM: 17)
+RELAY_PIN_2 = 20  # Ganti dengan pin GPIO yang sesuai (misalnya BCM: 18)
+RELAY_PIN_3 = 22  # Ganti dengan pin GPIO yang sesuai (misalnya BCM: 27)
+RELAY_PIN_4 = 23  # Ganti dengan pin GPIO yang sesuai (misalnya BCM: 22)
 
-# Inisialisasi GPIO
-gpio.init()
-gpio.setcfg(RELAY_PIN_1, gpio.OUTPUT)
+# Inisialisasi pin relay sebagai output
+wiringpi.pinMode(RELAY_PIN_1, wiringpi.OUTPUT)
+wiringpi.pinMode(RELAY_PIN_2, wiringpi.OUTPUT)
+wiringpi.pinMode(RELAY_PIN_3, wiringpi.OUTPUT)
+wiringpi.pinMode(RELAY_PIN_4, wiringpi.OUTPUT)
 
+# Fungsi untuk membaca data dari MPU-6050
 def read_mpu6050_data():
     def read_word(reg):
-        high = bus.read_byte_data(MPU6050_ADDR, reg)
-        low = bus.read_byte_data(MPU6050_ADDR, reg + 1)
+        high = wiringpi.wiringPiI2CReadReg8(fd, reg)
+        low = wiringpi.wiringPiI2CReadReg8(fd, reg + 1)
         value = (high << 8) + low
         if value >= 0x8000:
             value = -((65535 - value) + 1)
         return value
 
+    fd = wiringpi.wiringPiI2CSetup(MPU6050_ADDR)
     accel_x = read_word(MPU6050_REG_ACCEL_XOUT_H)
     accel_y = read_word(MPU6050_REG_ACCEL_YOUT_H)
     accel_z = read_word(MPU6050_REG_ACCEL_ZOUT_H)
-
     return accel_x, accel_y, accel_z
+
+# Fungsi untuk menggerakkan relay berdasarkan azimuth
+def control_relay(predicted_azimuth):
+    if predicted_azimuth < 90:
+        wiringpi.digitalWrite(RELAY_PIN_1, wiringpi.HIGH)
+        time.sleep(1)
+        wiringpi.digitalWrite(RELAY_PIN_1, wiringpi.LOW)
+    elif predicted_azimuth < 180:
+        wiringpi.digitalWrite(RELAY_PIN_2, wiringpi.HIGH)
+        time.sleep(1)
+        wiringpi.digitalWrite(RELAY_PIN_2, wiringpi.LOW)
+    elif predicted_azimuth < 270:
+        wiringpi.digitalWrite(RELAY_PIN_3, wiringpi.HIGH)
+        time.sleep(1)
+        wiringpi.digitalWrite(RELAY_PIN_3, wiringpi.LOW)
+    else:
+        wiringpi.digitalWrite(RELAY_PIN_4, wiringpi.HIGH)
+        time.sleep(1)
+        wiringpi.digitalWrite(RELAY_PIN_4, wiringpi.LOW)
+
+# Fungsi untuk menghitung azimuth berdasarkan data MPU-6050
+def calculate_azimuth(accel_x, accel_y, accel_z):
+    roll = atan2(accel_y, accel_z)
+    azimuth = degrees(roll)
+    return azimuth
 
 if __name__ == "__main__":
     try:
         while True:
+            # Baca data dari MPU-6050
             accel_x, accel_y, accel_z = read_mpu6050_data()
-            print(f"Accel X: {accel_x}, Accel Y: {accel_y}, Accel Z: {accel_z}")
 
-            gpio.output(RELAY_PIN_1, gpio.HIGH)
-            time.sleep(1)
-            gpio.output(RELAY_PIN_1, gpio.LOW)
-            time.sleep(1)
-    except Exception as e:
-        print(f"Error: {e}")
+            # Hitung azimuth dari data MPU-6050
+            azimuth = calculate_azimuth(accel_x, accel_y, accel_z)
+
+            # Kontrol relay berdasarkan azimuth yang diprediksi
+            control_relay(azimuth)
+
+            time.sleep(1)  # Tunggu sebelum pembacaan data berikutnya
+
+    except KeyboardInterrupt:
+        print("Program stopped by user")
     finally:
-        gpio.output(RELAY_PIN_1, gpio.LOW)
-        gpio.cleanup()
+        # Matikan semua relay
+        wiringpi.digitalWrite(RELAY_PIN_1, wiringpi.LOW)
+        wiringpi.digitalWrite(RELAY_PIN_2, wiringpi.LOW)
+        wiringpi.digitalWrite(RELAY_PIN_3, wiringpi.LOW)
+        wiringpi.digitalWrite(RELAY_PIN_4, wiringpi.LOW)
