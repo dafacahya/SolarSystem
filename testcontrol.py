38f176a3213276921fa2000754ecc0783af49325
root <root@DafaCahya>
2024-07-04 15:31:37 +0700

    update all

--- testcontrol.py
+++ testcontrol.py
@@ -1,8 +1,8 @@
 import wiringpi
 import time
-from math import atan2, degrees, sin, cos
 import pandas as pd
 import os
+from math import atan2, degrees, sin, cos
 
 # Alamat I2C dari MPU-6050
 MPU6050_ADDR = 0x68
